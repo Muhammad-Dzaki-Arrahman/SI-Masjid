4178676ade309ffe63b8a578c2ff9097f5c83fb2
Muhammad-Dzaki-Arrahman <78633671+Muhammad-Dzaki-Arrahman@users.noreply.github.com>
2022-10-11 21:47:05 +0700

    Add files via upload

---
+++ app/Controllers/SimController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\M_Login;
+
+class SimController extends BaseController
+{
+    public function index()
+    {  
+        return view('admin/login');
+    }
+    public function cek_login()
+    {
+        $modelLogin = new M_Login;
+
+        $username = $this->request->getPost('uname');
+        $cekUsername = $modelLogin->getAdmin(['username'=>$username])->getNumRows();
+        if($cekUsername == 0){
+            return redirect()->to('admin/login');
+        }else{
+            $dataUser = $modelLogin->getAdmin(['username'=>$username])->getRowArray();
+            $password = $this->request->getPost('password');
+            $verifyPassword = password_verify($password, $dataUser['password']);
+            if (!$verifyPassword) {
+                // code...
+                return view('admin/login');
+            }else{
+            return view('admin/dashboard');
+        }
+        }
+    }
+    public function dashboard()
+    {
+        return view('admin/dashboard');
+    }
+}
