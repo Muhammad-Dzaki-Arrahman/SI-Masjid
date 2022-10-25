22e5964ded8aa7267c0041af79ee3287e9180cfd
abiabagastufari <83526881+abiabagastufari@users.noreply.github.com>
2022-10-25 20:50:35 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -41,10 +41,14 @@ $routes->get('/dashboard', 'SimController::dashboard');
 $routes->get('/kegiatan', 'SimController::kegiatan');
 $routes->get('/pengumuman', 'SimController::pengumuman');
 $routes->get('/profile', 'SimController::profile');
+$routes->get('/kasmasuk', 'SimController::kasmasuk');
+$routes->get('/kaskeluar', 'SimController::kaskeluar');
 $routes->post('/cek-login', 'SimController::cek_login');
 $routes->post('/dashboard', 'SimController::dashboard');
 $routes->get('/ckegiatan', 'SimController::ckegiatan');
+$routes->post('/storekgt', 'CRUDController::storekgt');
 $routes->get('/cpengumuman', 'SimController::cpengumuman');
+$routes->post('/storepgn', 'CRUDController::storepgn');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
