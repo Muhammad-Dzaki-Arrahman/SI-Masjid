f221b944af3e408b063c8bfb37ce542839cb668f
abiabagastufari <83526881+abiabagastufari@users.noreply.github.com>
2022-11-07 17:17:38 +0700

    Update M_total.php

--- app/Models/M_total.php
+++ app/Models/M_total.php
@@ -1,12 +1,16 @@
 <?php
 
-
 namespace App\Models;
 
 use CodeIgniter\Model;
 
 class M_total extends Model {
         protected $table = 'tbl_total';
+        protected $primaryKey = 'id_total';
+        protected $useAutoIncrement = true;
+        protected $returnType       = 'array';
+        protected $useSoftDeletes   = false;
+        protected $protectFields    = true;
         protected $allowedFields    = ['tahun', 'bulan', 'jumlah_km', 'jumlah_kk', 'total'];
 
         public function getTotal($where = false){
