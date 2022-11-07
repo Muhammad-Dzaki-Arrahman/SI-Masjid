63bc62f91b1f3cac46b88e71901390681e781eb0
abiabagastufari <83526881+abiabagastufari@users.noreply.github.com>
2022-11-07 17:16:33 +0700

    Update M_kasmasuk.php

--- app/Models/M_kasmasuk.php
+++ app/Models/M_kasmasuk.php
@@ -7,6 +7,12 @@ use CodeIgniter\Model;
 
 class M_kasmasuk extends Model {
         protected $table = 'tbl_kasmasuk';
+        protected $primaryKey = 'id_kasmasuk';
+        protected $useAutoIncrement = true;
+        protected $returnType       = 'array';
+        protected $useSoftDeletes   = false;
+        protected $protectFields    = true;
+        protected $allowedFields    = ['nama', 'jumlah', 'tanggal', 'keterangan'];
 
         public function getKasmasuk($where = false){
             if($where === false){
