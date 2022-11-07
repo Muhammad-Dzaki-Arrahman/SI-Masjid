82528824f20723073f74f5289be22bb434204b5e
abiabagastufari <83526881+abiabagastufari@users.noreply.github.com>
2022-11-07 17:16:53 +0700

    Update M_kaskeluar.php

--- app/Models/M_kaskeluar.php
+++ app/Models/M_kaskeluar.php
@@ -7,6 +7,12 @@ use CodeIgniter\Model;
 
 class M_kaskeluar extends Model {
         protected $table = 'tbl_kaskeluar';
+        protected $primaryKey = 'id_kaskeluar';
+        protected $useAutoIncrement = true;
+        protected $returnType       = 'array';
+        protected $useSoftDeletes   = false;
+        protected $protectFields    = true;
+        protected $allowedFields    = ['nama', 'jumlah', 'tanggal', 'keterangan'];
 
         public function getKaskeluar($where = false){
             if($where === false){
