efda32be55f31d57d420d3079de90fca4e7dcbd5
abiabagastufari <83526881+abiabagastufari@users.noreply.github.com>
2022-11-07 17:11:07 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,24 +35,54 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+
+//Admin
 $routes->get('/', 'Home::index');
 $routes->get('/login', 'SimController::index');
 $routes->get('/dashboard', 'SimController::dashboard');
 $routes->get('/kegiatan', 'SimController::kegiatan');
 $routes->get('/pengumuman', 'SimController::pengumuman');
 $routes->get('/profile', 'SimController::profile');
+$routes->get('/kasmasuk', 'SimController::kasmasuk');
+$routes->get('/ckasmasuk', 'SimController::ckasmasuk');
+$routes->post('/storeksm', 'CRUDController::storeksm');
+$routes->get('/kaskeluar', 'SimController::kaskeluar');
+$routes->get('/ckaskeluar', 'SimController::ckaskeluar');
+$routes->post('/storeksl', 'CRUDController::storeksl');
+$routes->get('/zakat', 'SimController::zakat');
+$routes->get('/czakat', 'SimController::czakat');
+$routes->post('/storezkt', 'CRUDController::storezkt');
+$routes->get('/total', 'SimController::total');
+$routes->get('/ctotal', 'SimController::ctotal');
+$routes->post('/storettl', 'CRUDController::storettl');
 $routes->post('/cek-login', 'SimController::cek_login');
 $routes->post('/dashboard', 'SimController::dashboard');
 $routes->get('/ckegiatan', 'SimController::ckegiatan');
+$routes->post('/storekgt', 'CRUDController::storekgt');
 $routes->get('/cpengumuman', 'SimController::cpengumuman');
-$routes->post('/admin/save-pengumuman', 'SimController::save_p');
-$routes->post('/admin/save-kegiatan', 'SimController::save_k');
+$routes->post('/storepgn', 'CRUDController::storepgn');
 $routes->get('/admin/delete-userk/(:num)', 'SimController::delete_k/$1');
 $routes->get('/admin/delete-userp/(:num)', 'SimController::delete_p/$1');
 $routes->get('/admin/crud/edit_k/(:num)', 'SimController::edit_k/$1');
 $routes->get('/admin/crud/edit_p/(:num)', 'SimController::edit_p/$1');
 $routes->post('/admin/update-userk/(:num)', 'SimController::update_k/$1');
 $routes->post('/admin/update-userp/(:num)', 'SimController::update_p/$1');
+$routes->post('/admin/update-userkm/(:num)', 'SimController::update_km/$1');
+$routes->get('/admin/crud/edit_km/(:num)', 'SimController::edit_km/$1');
+$routes->get('/admin/delete-userkm/(:num)', 'SimController::delete_km/$1');
+$routes->post('/admin/update-userkk/(:num)', 'SimController::update_kk/$1');
+$routes->get('/admin/crud/edit_kk/(:num)', 'SimController::edit_kk/$1');
+$routes->get('/admin/delete-userkk/(:num)', 'SimController::delete_kk/$1');
+$routes->post('/admin/update-userz/(:num)', 'SimController::update_z/$1');
+$routes->get('/admin/crud/edit_z/(:num)', 'SimController::edit_z/$1');
+$routes->get('/admin/delete-userz/(:num)', 'SimController::delete_z/$1');
+$routes->post('/admin/update-usert/(:num)', 'SimController::update_t/$1');
+$routes->get('/admin/crud/edit_t/(:num)', 'SimController::edit_t/$1');
+$routes->get('/admin/delete-usert/(:num)', 'SimController::delete_t/$1');
+
+//User
+$routes->get('/home', 'User::home');
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
