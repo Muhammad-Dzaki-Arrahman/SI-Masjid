e29c1fbccdc7075220814d4fba097673920f096d
denyaldyaprizaaa <83523347+denyaldyaprizaaa@users.noreply.github.com>
2022-11-22 10:02:31 +0700

    Update berita.php

--- app/Views/admin/berita.php
+++ app/Views/admin/berita.php
@@ -81,7 +81,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="http://localhost:8081/login">
+          <a class="nav-link " href="login">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="ni ni-button-power text-dark text-sm opacity-10"></i>
             </div>
